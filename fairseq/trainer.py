be2709efea944e5becd4dbb947a0c75c819a70dc
Songyang Zhang <szhang83@ur.rochester.edu>
2022-07-07 20:57:25 -1000

    Update trainer.py

--- fairseq/trainer.py
+++ fairseq/trainer.py
@@ -580,16 +580,18 @@ class Trainer(object):
                         )
                         layer._prune_fc_layer(remove_index=remove_index)
                     logger.info(self.model)
-                strict = True
-                if getattr(self.cfg.model, "finetune_from_image_model", False): strict = False
+
+                if getattr(self.cfg.model, "finetune_from_image_model", False):
+                    self.model.use_sharded_state = False
+                    self.model.load_state_dict(state["model"], strict=False, model_cfg=self.cfg.model)
+                    self.model.use_sharded_state = True
+                else:
+                    self.model.load_state_dict(state["model"], strict=True, model_cfg=self.cfg.model)
                 # if self.cfg.checkpoint.load_ema_checkpoint:
                 #     logger.info(f"Loading EMA checkpoint from {filename}")
                 #     ema_model_state_dict = checkpoint_utils.load_ema_from_checkpoint(filename)["model"]
                 #     self.model.load_state_dict(copy.deepcopy(ema_model_state_dict), strict=strict, model_cfg=self.cfg.model)
                 # else:
-                self.model.use_sharded_state = False
-                self.model.load_state_dict(state["model"], strict=strict, model_cfg=self.cfg.model)
-                self.model.use_sharded_state = True
                 # save memory for later steps
                 del state["model"]
                 if utils.has_parameters(self.get_criterion()):
